ddc43ab74b703e4e7b151646844a30ad6e2231a4
kurtjohn21 <kjmojado21@gmail.com>
2024-01-16 16:58:10 +0800

    added hanges in database

--- classes/Database.php
+++ classes/Database.php
@@ -11,7 +11,7 @@
             $this->conn = new mysqli($this->server_name, $this->db_username, $this->db_password, $this->db_name);
 
             if($this->conn->connect_error){
-                die("Unable to connect to database " . $this->db_name . " : ". $this->conn->connect_error);
+                die("Unable to connect to databases " . $this->db_name . " : ". $this->conn->connect_error);
             }
         }
     }
